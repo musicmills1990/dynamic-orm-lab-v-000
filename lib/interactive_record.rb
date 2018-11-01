9decfec6fc326926f0ef4e6851c7b98f76d554fd
Marc Mills <mdmills1990@gmail.com>
2018-11-01 18:24:16 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,9 +2,20 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-  
+
   def self.table_name
     self.to_s.downcase.pluralize
   end
 
+  def self.column_names
+    DB[:conn].results_as_hash = true
+
+    sql = "PRAGMA table_info('#{table_name}')"
+    table_info = DB[:conn].execute(sql)
+    binding.pry
+    column_names = []
+    table_into.each do |row|
+      column_names << row["name"]
+    end
+    column_names.compact
 end
